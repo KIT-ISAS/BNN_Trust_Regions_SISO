096a877bd8b7f22acadae99bf12723271121cb80
WalkerMarkus <markus.walker@kit.edu>
2023-12-05 21:16:50 +0100

    add doc string

--- bnn_trust_regions/io_data.py
+++ bnn_trust_regions/io_data.py
@@ -6,5 +6,13 @@ import numpy as np
 
 @dataclass
 class IOData:
+    """
+    Represents input-output data for a model.
+
+    :param input: The input data for the model.
+    :type input: np.ndarray
+    :param output: The output data for the model.
+    :type output: np.ndarray
+    """
     input: np.ndarray
     output: np.ndarray
