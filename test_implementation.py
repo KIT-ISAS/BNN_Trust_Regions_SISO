ce250fdcda79e4789204ba3cbddac5f3a11ea33e
WalkerMarkus <markus.walker@kit.edu>
2023-12-06 14:27:31 +0100

    test funtionality

--- test_implementation.py
+++ test_implementation.py
@@ -1,20 +1,19 @@
-import numpy as np
 
-import bnn_trust_regions
-
-from bnn_trust_regions.model_evaluator import ModelEvaluator
+from bnn_trust_regions.model_evaluator import ModelEvaluator, UseAorB
 from bnn_trust_regions.wasserstein_dist import WassersteinDistance
 from bnn_trust_regions.gaussian import UnivariateGaussian
 from bnn_trust_regions.io_data import IOData
-from bnn_trust_regions.candidate_region_identification import IdentGifSettings, CandidateRegionIdentification
+from bnn_trust_regions.candidate_region_identification import IdentGifSettings, SisoCandidateRegionIdentification
+from bnn_trust_regions.stat_test_settings import StatTestSettings
 
 from bnn_trust_regions.utils import save_load
 
 
 def test_functionality():
 
-    np.random.seed(42)
-
+    ########################################################################################################
+    # load test data
+    ########################################################################################################
     # folder with some example data and predictions
     data_folder = "example_data"
     # load test data
@@ -35,37 +34,92 @@ def test_functionality():
     pbp_file_name = "pbp_test"
     pred_pbp_mean, pred_pbp_var = save_load.load_sampled_predictions(data_folder, pbp_file_name)
     pred_pbp = UnivariateGaussian(mean=pred_pbp_mean, var=pred_pbp_var)
+    ########################################################################################################
 
+    ########################################################################################################
+    #  change predictions and test data and hyperparameters
+    # for region identification and statistical testing here
+    ########################################################################################################
     # evaluate predictions from mcmc and svi
     pred_a = pred_mcmc  # mcmc as reference model
     pred_b1 = pred_svi  # svi as approximation model
 
-    # set wasserstein distance settings
-    ws_dist_settings1 = WassersteinDistance(p_norm=1, parallel_computing=True, verbose=False)
+    pred_b2 = pred_pbp  # pbp as approximation model
 
+    # set wasserstein distance settings
+    p_norm = 1
+    parallel_computing = True
+    verbose = False
+    ws_dist_settings1 = WassersteinDistance(
+        p_norm=p_norm, parallel_computing=parallel_computing, verbose=verbose)
+    ws_dist_settings2 = WassersteinDistance(p_norm=1, parallel_computing=False, verbose=False)
+
+    # candidate region identification settings
+    min_points_per_region = 200
+    smoothing_window_size = 50
+    plot_gif = False  # True or False
+
+    # display gif of regions identification critical distance
+    plot_folder1 = "eval1_plots"
+    plot_folder2 = "eval2_plots"
+    file_name = "crit_dist"
+    dpi = 200
+    fps = 2
+    loop = 0  # 0 for infinite loop
+    gif_settings1 = IdentGifSettings(
+        path=plot_folder1, file_name=file_name, dpi=dpi, fps=fps, loop=loop)
+    gif_settings2 = IdentGifSettings(
+        path=plot_folder2, file_name=file_name, dpi=dpi, fps=fps, loop=loop)
+
+    region_ident1 = SisoCandidateRegionIdentification(
+        min_points_per_region=min_points_per_region, smoothing_window_size=smoothing_window_size,
+        verbose=plot_gif, gif_settings=gif_settings1)
+    region_ident2 = SisoCandidateRegionIdentification(
+        min_points_per_region=min_points_per_region, smoothing_window_size=smoothing_window_size,
+        verbose=plot_gif, gif_settings=gif_settings2)
+
+    # statistical test settings
+    alpha = 0.01  # significance level of 1%
+    confidence_interval = 0.95  # test the 95% confidence interval
+    stat_test_settings = StatTestSettings(alpha=alpha, confidence_interval=confidence_interval)
+
+    # test model A or B
+    use_a_or_b = UseAorB.B
+    ########################################################################################################
+
+    ########################################################################################################
     # evaluate predictions from mcmc and svi
+    ########################################################################################################
     model_evaluator1 = ModelEvaluator(
         predictions_a=pred_a, predictions_b=pred_b1, wasserstein_distance=ws_dist_settings1, test_data=test_data)
 
     # wasserstein distance between reference and approximation model
     model_evaluator1.calc_wasserstein_distance()
 
-    gif_settings = IdentGifSettings(
-        path="eval1_plots", file_name="crit_dist", dpi=200, fps=2, loop=0)
     # calculate candidate regions and plot gif of critical distance
-    region_ident = CandidateRegionIdentification(
-        min_points_per_region=200, smoothing_window_size=50, verbose=True, gif_settings=gif_settings)
-    model_evaluator1.calc_canidate_regions(region_ident=region_ident)
+    model_evaluator1.calc_canidate_regions(region_ident=region_ident1)
+    model_evaluator1.calc_statistical_tests(
+        stat_test_settings=stat_test_settings, use_a_or_b=use_a_or_b)
+    model_evaluator1.print_statistical_tests()  # print results to console
+    ########################################################################################################
 
+    ########################################################################################################
     # evaluate predictions from mcmc and pbp
     # mcmc as reference model
-    pred_b2 = pred_pbp  # pbp as approximation model
+    # pred_b2 = pred_pbp  # pbp as approximation model
+    ########################################################################################################
 
-    ws_dist_settings2 = WassersteinDistance(p_norm=1, parallel_computing=False, verbose=False)
     model_evaluator2 = ModelEvaluator(
         predictions_a=pred_a, predictions_b=pred_b2, test_data=test_data)
+
     model_evaluator2.set_distance_settings(ws_dist_settings2)
     model_evaluator2.calc_wasserstein_distance()
+
+    model_evaluator2.calc_canidate_regions(region_ident=region_ident2)
+    model_evaluator2.calc_statistical_tests(
+        stat_test_settings=stat_test_settings, use_a_or_b=use_a_or_b)
+    model_evaluator2.print_statistical_tests()  # print results to console
+    ########################################################################################################
 
 
 if __name__ == "__main__":
