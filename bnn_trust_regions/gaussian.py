b9cf83d03e0bdd0b081fc5af203c76cf92bf20a1
WalkerMarkus <markus.walker@kit.edu>
2023-12-06 14:29:47 +0100

    add quantile method to univariate gaussian

--- bnn_trust_regions/gaussian.py
+++ bnn_trust_regions/gaussian.py
@@ -1,8 +1,11 @@
 """ class UnivariateGaussian: parametes of univariate Gaussian distributions"""
 import dataclasses
 import typing
+from tqdm import tqdm
 
 import numpy as np
+import scipy.stats
+
 
 @dataclasses.dataclass
 class UnivariateGaussian:
@@ -15,3 +18,21 @@ class UnivariateGaussian:
     """
     mean: typing.Union[float, np.ndarray]
     var: typing.Union[float, np.ndarray]
+
+    def get_gaussian_quantiles(self, quantiles_lower_tail_probability: np.ndarray):
+        """
+        Calculates Gaussian quantiles based on the lower tail probability.
+
+        :param quantiles_lower_tail_probability: Array of lower tail probabilities for which to calculate Gaussian quantiles.
+        :type quantiles_lower_tail_probability: numpy.ndarray
+        :return: Array of Gaussian quantile positions.
+        :rtype: numpy.ndarray
+        """
+        num_elements = len(self.mean)
+        num_quantiles = len(quantiles_lower_tail_probability)
+        std = np.sqrt(self.var)
+        quantile_position = np.empty((num_quantiles, num_elements))
+        for i in tqdm(range(0, num_elements), desc="Progress", disable=True):
+            quantile_position[:, i] = scipy.stats.norm.ppf(
+                quantiles_lower_tail_probability, loc=self.mean[i], scale=std[i])
+        return quantile_position
