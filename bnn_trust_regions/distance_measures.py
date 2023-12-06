b51bf59ffeeebdf7844c2c4bbc26e3b9f8638882
WalkerMarkus <markus.walker@kit.edu>
2023-12-06 14:28:56 +0100

    fix bug

--- bnn_trust_regions/distance_measures.py
+++ bnn_trust_regions/distance_measures.py
@@ -6,23 +6,7 @@ import typing
 import numpy as np
 
 
-from gaussian import UnivariateGaussian
-
-
-def mean_square_error(x_pred, x_observed):
-    """
-    Calculates the mean squared error between predicted and observed values.
-
-    :param x_pred: A numpy array of predicted values.
-    :type x_pred: numpy.ndarray
-    :param x_observed: A numpy array of observed values.
-    :type x_observed: numpy.ndarray
-    :return: The mean squared error between the predicted and observed values.
-    :rtype: float
-    """
-    if isinstance(x_pred, UnivariateGaussian):
-        x_pred = x_pred.mean
-    return np.square(x_pred - x_observed.T).mean(axis=None)
+from .gaussian import UnivariateGaussian
 
 
 def squared_mahalanobis_distance(prediction: typing.Union[np.ndarray, UnivariateGaussian], output_data: np.ndarray,):
@@ -40,7 +24,7 @@ def squared_mahalanobis_distance(prediction: typing.Union[np.ndarray, Univariate
     """
     if isinstance(prediction, UnivariateGaussian):
         mean_prediction = prediction.mean
-        var_prediction = prediction.cov
+        var_prediction = prediction.var
     else:
         mean_prediction = np.mean(prediction, axis=0)
         var_prediction = np.var(prediction, axis=0)
