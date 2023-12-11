8d08e2722a316a2c887aead523723403ea874c98
WalkerMarkus <markus.walker@kit.edu>
2023-12-11 15:51:03 +0100

    add init

--- bnn_trust_regions/gaussian.py
+++ bnn_trust_regions/gaussian.py
@@ -19,6 +19,32 @@ class UnivariateGaussian:
     mean: typing.Union[float, np.ndarray]
     var: typing.Union[float, np.ndarray]
 
+    def __init__(self, mean: typing.Union[float, np.ndarray], var: typing.Union[float, np.ndarray]):
+        """
+        Initializes a univariate Gaussian distribution.
+
+        :param mean: The mean of the distribution. Can be a single value or a (1, n) vector of means.
+        :type mean: Union[float, np.ndarray]
+        :param var: The variance of the distribution. Can be a single value or a (1, n) vector of variances.
+        :type var: Union[float, np.ndarray]
+        """
+        if not isinstance(mean, np.ndarray):
+            mean = np.array([mean])
+
+        self.mean = mean
+        self.var = var
+
+        # if mean is array and var is float, make var array
+        if isinstance(mean, np.ndarray) and isinstance(var, (float, int)):
+            self.var = np.full_like(mean, var)
+
+        # set mean and var to 1d arrays
+        # if self.mean.ndim == 1:
+        #     self.mean = self.mean.reshape(1, -1)
+
+        # if self.var.ndim == 1:
+        #     self.var = self.var.reshape(1, -1)
+
     def get_gaussian_quantiles(self, quantiles_lower_tail_probability: np.ndarray):
         """
         Calculates Gaussian quantiles based on the lower tail probability.
